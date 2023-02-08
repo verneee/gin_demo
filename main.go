e52fef8e4f032722223f523b2d99edb71ea9c623
kaiguo <kguo175@gmail.com>
2023-02-08 22:05:48 +0800

    gin_demo

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	file, _ := ioutil.ReadFile("./hello.txt")
+	fmt.Fprintf(w, string(file))
+}
+func main() {
+	http.HandleFunc("/hello", sayHello)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Printf("http serve faild  err:%v\n", err)
+		return
+	}
 }
